e4388c7cbf31785a7b2c6132d1c48eaddf75a74c
Aaron Zhang <zhangzeyu99@gmail.com>
2026-09-14 00:26:58 +0800

    chore(miniapp): pin response provenance core revision

--- miniapp/src/version.js
+++ miniapp/src/version.js
@@ -1,6 +1,6 @@
 export const MINIAPP_VERSION = "1.1.16";
 export const MINIAPP_UPDATE_DATE = "2026-09-13";
 export const WEB_CORE_VERSION = "2.0.2";
-export const WEB_CORE_REVISION = "180a83d613e717636778bf98cc51a0433d9119ef";
+export const WEB_CORE_REVISION = "c708d88edae021c37e9065e520ecedc739f8bfd2";
 export const MINIAPP_RELEASE_LABEL =
   `小程序 v${MINIAPP_VERSION} · 网页核心 v${WEB_CORE_VERSION}`;
